2ebf91f8139fe2be8514c0b4c43d3338e8b5986b
spidroid381 <102158589+spidroid3810@users.noreply.github.com>
2024-10-20 09:31:03 +0530

    Update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -9,7 +9,7 @@ model.load_state_dict(torch.load('model_reduced.safetensors'))
 model.eval()
 
 # Text input (you can change this to any text from your dataset)
-text_input = "deadpool in action"  # Change this to test other descriptions
+text_input = "A deadpool"  # Change this to test other descriptions
 
 # Simple char-to-int encoding (max_len=8)
 max_len = 8
